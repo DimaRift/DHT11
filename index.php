cc3986d8b56ba51817c72177b8f4da633265e529
adrian <scouby42@live.fr>
2018-11-30 16:32:40 +0100

    debut

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
-<link rel="stylesheet" type="text/css" href="main.css">
+
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
 <h1>Température</h1>
 <?php
 $homepage = file_get_contents('data.txt');
 echo $homepage;
+
+$data = json_decode($homepage);
+
+
 ?>
-<img src="img/thermometer.jpg">
+<p> il fait <?php echo $data->temperature;?> avec 46% d'humidité</p>
+<div id="thermometer">
+  <div id="bargraph"></div>
+</div>
 </body>
 </html>
