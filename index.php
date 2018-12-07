bc82a1175b23cea04d2e3b07aadf8ca69d2c8b85
adrian <scouby42@live.fr>
2018-12-07 13:49:16 +0100

    debut

--- index.php
+++ index.php
@@ -1,23 +1,34 @@
 <!DOCTYPE html>
-
 <html>
 <head>
+<meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="main.css"></link>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<h1>Température</h1>
+
+
+<h1 >Température</h1>
+
 <?php
-$homepage = file_get_contents('data.txt');
-echo $homepage;
 
-$data = json_decode($homepage);
-
+$file = "data.txt";
+$texte = file_get_contents($file);
+$data = json_decode($texte);
+$bargraph_height = 160+ $data->temperature * 4;
+$bargraph_top = 315+$data->temperature * 4*-1;
 
 ?>
-<p> il fait <?php echo $data->temperature;?> avec 46% d'humidité</p>
+<p>il fait <?php echo $data->temperature; ?>° avec <?php echo $data->humidite ?>% d'humidité</p>
+
 <div id="thermometer">
-  <div id="bargraph"></div>
+
+  <div id="bargraph" style=<?php echo "\"height:".$bargraph_height."px; top:".$bargraph_top."px;\""?>
+
+	>
+  </div>
 </div>
+
 </body>
 </html>
